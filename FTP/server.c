5a377528f8ee02f649e25c1008e86f5a9601e8ff
Krishnanand <krishnanandunnikrishnan@gmail.com>
2023-06-01 22:21:17 +0530

    added stopnwait arq

--- FTP/server.c
+++ FTP/server.c
@@ -21,44 +21,36 @@ int main() {
     FILE *fp;
     size_t bytesRead;
 
-    // Create socket(here AF_INET represent IPV4,SOCK STREAM represent TCP and 0 is a paramenter specific to type of socket made!)
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket < 0)
         error("Error opening socket.");
 
-    // Set server address  
     memset(&serverAddress, 0, sizeof(serverAddress));  //sets serverAddress structure values to 0 for all the default structure props
     serverAddress.sin_family = AF_INET;                 //IPV4
     serverAddress.sin_port = htons(PORT);       //PORT IS SET
     serverAddress.sin_addr.s_addr = INADDR_ANY;  //TO RECEIVE ALL TYPES OF CONNECTION(ANY ADDRESS)
 
-    // Bind the socket to the specified port
     if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)   //BINDS SERVER ADDRESS TO SERVERSOCKET AND CHECKS IF BINDING WAS SUCCESSFULL OR NOT
         error("Error binding socket.");
 
-    // Listen for client connections
     if (listen(serverSocket, 5) < 0)                                 //LISTENING TO INCOMING CLIENT CONNECTIONS
         error("Error listening for connections.");
 
     printf("Server started. Listening on port %d...\n", PORT);
 
-    // Accept incoming connection
     clientLength = sizeof(clientAddress);
     clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientLength);
     if (clientSocket < 0)
         error("Error accepting connection.");
 
-    // Open file to save received data
     fp = fopen("received_file.txt", "w");
     if (fp == NULL)
         error("Error opening file to save received data.");
 
-    // Receive data and write to the file
     while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
         fwrite(buffer, 1, bytesRead, fp);
     }
 
-    // Close file and sockets
     fclose(fp);
     close(clientSocket);
     close(serverSocket);
---
+++ slidingwindowprotocols/STOPN/stopandwait.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <stdlib.h>  // rand(), srand()
+#include <time.h>    // time()
+
+// Simulating sending a frame
+void sendFrame(int frameNumber) {
+    printf("Sending frame: %d\n", frameNumber);
+}
+
+// Simulating receiving an acknowledgment
+bool receiveAck() {
+    // Simulating a random ACK generation
+    srand(time(NULL));
+    return rand() % 2 == 0;
+}
+
+int main() {
+    int frameNumber = 0;
+    
+    while (true) {
+        // Send the frame
+        sendFrame(frameNumber);
+        
+        // Wait for acknowledgment
+        sleep(1);  // Simulating network delay
+        
+        if (receiveAck()) {
+            // Frame acknowledged, move to the next frame
+            frameNumber++;
+        } else {
+            // Frame not acknowledged, retransmit the current frame
+            printf("Frame not acknowledged. Retransmitting frame: %d\n", frameNumber);
+        }
+    }
+    
+    return 0;
+}
