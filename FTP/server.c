e61655b914ea3ce798308dc4b7d7a044185ac07d
Krishnanand <krishnanandunnikrishnan@gmail.com>
2023-06-01 15:37:02 +0530

    commented serverside code

--- FTP/server.c
+++ FTP/server.c
@@ -21,23 +21,23 @@ int main() {
     FILE *fp;
     size_t bytesRead;
 
-    // Create socket
+    // Create socket(here AF_INET represent IPV4,SOCK STREAM represent TCP and 0 is a paramenter specific to type of socket made!)
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket < 0)
         error("Error opening socket.");
 
-    // Set server address
-    memset(&serverAddress, 0, sizeof(serverAddress));
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(PORT);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    // Set server address  
+    memset(&serverAddress, 0, sizeof(serverAddress));  //sets serverAddress structure values to 0 for all the default structure props
+    serverAddress.sin_family = AF_INET;                 //IPV4
+    serverAddress.sin_port = htons(PORT);       //PORT IS SET
+    serverAddress.sin_addr.s_addr = INADDR_ANY;  //TO RECEIVE ALL TYPES OF CONNECTION(ANY ADDRESS)
 
     // Bind the socket to the specified port
-    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)   //BINDS SERVER ADDRESS TO SERVERSOCKET AND CHECKS IF BINDING WAS SUCCESSFULL OR NOT
         error("Error binding socket.");
 
     // Listen for client connections
-    if (listen(serverSocket, 5) < 0)
+    if (listen(serverSocket, 5) < 0)                                 //LISTENING TO INCOMING CLIENT CONNECTIONS
         error("Error listening for connections.");
 
     printf("Server started. Listening on port %d...\n", PORT);
